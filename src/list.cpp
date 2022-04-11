5814f5861b2d30c209b65441be9de08140379a35
ueliot <ueliot2001@yahoo.com>
2022-04-11 13:07:37 +0200

    Añadido delete

--- src/list.cpp
+++ src/list.cpp
@@ -49,7 +49,10 @@ void List::delete_item(){
   if(list.size()){
     for (int i = 0; i < (int)list.size(); i++){
       cout << i << ": " << list[i] << "\n";
-    }    
+    } 
+    int choiceNum;
+    cin >> choiceNum;
+    list.erase(list.begin()+choiceNum);  
   }
   else {
     cout << " No items to delete. \n";
